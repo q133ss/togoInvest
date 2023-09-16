a294ac23803279c3cc057b325dc6bc330315ff30
Alexey <test@git.ru>
2023-09-16 05:50:07 +0300

    first screen and header

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import Header from "./components/Header.vue";
+</script>
+
+<template>
+  <section class="main__section">
+    <Header></Header>
+    <div class="main__screen-content">
+      <div class="main__screen-content--wrap">
+        <h1 class="main__screen-content--title">
+          Увеличьте свой капитал и достигайте финансовых вершин
+        </h1>
+        <div class="main__screen-content--description-wrap">
+          <div class="main__screen-content--rectangle"></div>
+          <div class="main__screen-content--description">
+          Наша команда — группа экспертов <br>
+          по привлечению инвестиций для развития вашего дела.
+          </div>
+        </div>
+        <a href="#" class="main__screen-btn">
+          Привлечь инвестиции
+        </a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+</script>
+
+<template>
+<nav class="header__nav">
+  <div class="header__left-wrap">
+  <span class="logo__text">
+    TOGO
+    <span class="logo__primary-text">
+      invest
+    </span>
+  </span>
+
+  <ul class="menu">
+    <li class="header__menu-item">
+      Инвесторам
+    </li>
+    <li class="header__menu-item">
+      Партнерам
+    </li>
+    <li class="header__menu-item">
+      Контакты
+    </li>
+  </ul>
+  </div>
+  <div class="header__phone">
+    <img src="/img/Phone.png" alt="" class="header__phone-img">
+    <a href="tel:+62123412008" class="header__phone-text">+62 123-412-008</a>
+  </div>
+</nav>
+</template>
+
+<style scoped>
+
+</style>
